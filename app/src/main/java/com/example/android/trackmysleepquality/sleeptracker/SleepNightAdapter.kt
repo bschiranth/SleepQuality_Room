14f5f52e4b19a922149441e9442567cb2a652647
bschiranth <bschiranth@gmail.com>
2019-08-10 12:25:40 -0500

    refactor to move bind to holder class

--- app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
@@ -1,5 +1,6 @@
 package com.example.android.trackmysleepquality.sleeptracker
 
+import android.content.res.Resources
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import android.widget.TextView
@@ -18,28 +19,13 @@ class SleepNightAdapter : RecyclerView.Adapter<SleepViewHolder>() {
         }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int):  SleepViewHolder {
-        val layoutInflater = LayoutInflater.from(parent.context)
-        val view = layoutInflater.inflate(R.layout.list_sleep_item_view, parent, false)
-        return SleepViewHolder(view)
+        return SleepViewHolder.from(parent)
     }
 
     override fun getItemCount(): Int = data.size
 
     override fun onBindViewHolder(holder: SleepViewHolder, position: Int) {
         val dataItem = data[position]
-        val res = holder.itemView.context.resources
-
-        holder.sleepLength.text = convertDurationToFormatted(dataItem.startTimeMilli, dataItem.endTimeMilli, res)
-        holder.quality.text = convertNumericQualityToString(dataItem.sleepQuality, res)
-
-        holder.qualityImage.setImageResource(when (dataItem.sleepQuality) {
-            0 -> R.drawable.ic_sleep_0
-            1 -> R.drawable.ic_sleep_1
-            2 -> R.drawable.ic_sleep_2
-            3 -> R.drawable.ic_sleep_3
-            4 -> R.drawable.ic_sleep_4
-            5 -> R.drawable.ic_sleep_5
-            else -> R.drawable.ic_sleep_active
-        })
+        holder.bind(holder, dataItem)
     }
 }
--- app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepViewHolder.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepViewHolder.kt
@@ -1,16 +1,44 @@
 package com.example.android.trackmysleepquality.sleeptracker
 
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.android.trackmysleepquality.R
+import com.example.android.trackmysleepquality.convertDurationToFormatted
+import com.example.android.trackmysleepquality.convertNumericQualityToString
+import com.example.android.trackmysleepquality.database.SleepNight
 import kotlinx.android.synthetic.main.list_sleep_item_view.view.*
 
-class SleepViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+class SleepViewHolder private constructor(view: View) : RecyclerView.ViewHolder(view) {
 
     val sleepLength: TextView = itemView.findViewById(R.id.sleep_length)
     val quality: TextView = itemView.findViewById(R.id.quality_string)
     val qualityImage: ImageView = itemView.findViewById(R.id.quality_image)
 
+    fun bind(holder: SleepViewHolder, dataItem: SleepNight) {
+        val res = holder.itemView.context.resources
+        holder.sleepLength.text = convertDurationToFormatted(dataItem.startTimeMilli, dataItem.endTimeMilli, res)
+        holder.quality.text = convertNumericQualityToString(dataItem.sleepQuality, res)
+
+        holder.qualityImage.setImageResource(when (dataItem.sleepQuality) {
+            0 -> R.drawable.ic_sleep_0
+            1 -> R.drawable.ic_sleep_1
+            2 -> R.drawable.ic_sleep_2
+            3 -> R.drawable.ic_sleep_3
+            4 -> R.drawable.ic_sleep_4
+            5 -> R.drawable.ic_sleep_5
+            else -> R.drawable.ic_sleep_active
+        })
+    }
+
+    companion object {
+        fun from(parent: ViewGroup): SleepViewHolder {
+            val layoutInflater = LayoutInflater.from(parent.context)
+            val view = layoutInflater.inflate(R.layout.list_sleep_item_view, parent, false)
+            return SleepViewHolder(view)
+        }
+    }
 }
