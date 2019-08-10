fdaa98fa6bf891b4f7eae2df67d0f37bbcda74ff
bschiranth <bschiranth@gmail.com>
2019-08-10 13:26:50 -0500

    removing unsued param

--- app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
@@ -1,15 +1,8 @@
 package com.example.android.trackmysleepquality.sleeptracker
 
-import android.content.res.Resources
-import android.view.LayoutInflater
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
-import androidx.recyclerview.widget.RecyclerView
-import com.example.android.trackmysleepquality.R
-import com.example.android.trackmysleepquality.convertDurationToFormatted
-import com.example.android.trackmysleepquality.convertNumericQualityToString
 import com.example.android.trackmysleepquality.database.SleepNight
 
 class SleepNightAdapter : ListAdapter<SleepNight, SleepViewHolder>(SleepNightDiffUtilCallback()) {
@@ -20,7 +13,7 @@ class SleepNightAdapter : ListAdapter<SleepNight, SleepViewHolder>(SleepNightDif
 
     override fun onBindViewHolder(holder: SleepViewHolder, position: Int) {
         val dataItem = getItem(position)
-        holder.bind(holder, dataItem)
+        holder.bind(dataItem)
     }
 }
 
--- app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepViewHolder.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepViewHolder.kt
@@ -2,18 +2,13 @@ package com.example.android.trackmysleepquality.sleeptracker
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
-import com.example.android.trackmysleepquality.R
-import com.example.android.trackmysleepquality.convertDurationToFormatted
-import com.example.android.trackmysleepquality.convertNumericQualityToString
 import com.example.android.trackmysleepquality.database.SleepNight
 import com.example.android.trackmysleepquality.databinding.ListSleepItemViewBinding
 
 class SleepViewHolder private constructor(val binding: ListSleepItemViewBinding) : RecyclerView.ViewHolder(binding.root) {
 
-    fun bind(holder: SleepViewHolder, dataItem: SleepNight) {
+    fun bind(dataItem: SleepNight) {
         binding.sleep = dataItem
         binding.executePendingBindings()
     }
